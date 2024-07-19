b08cc3b017f17aa6ad9301ff272c9fe0dfcad42e
Thanh Dau <hello.thanhdauxuan@gmail.com>
2024-07-20 05:48:02 +0700

    Update get duplicate scores

--- server/__init__.py
+++ server/__init__.py
@@ -17,17 +17,22 @@ def hello(param):
    cond = re.compile(r"\d{6}")
    if not re.fullmatch(cond, param):
       html = '<p class="text-danger">Vui lòng nhập số báo danh hợp lệ!</p>'
-      return html
+      return [html]
    scores = cur.execute(f'SELECT * FROM scores WHERE scores.ID = {param};').fetchall()
    app.logger.debug(f"GOT IN DB: {scores}")
-   conn.close()
 
    if scores:
       data = scores[0]
+
+      same_score = cur.execute(f'SELECT count(*) FROM scores WHERE scores.Tong = {data[5]};').fetchone()
+      if same_score:
+         same_score = same_score[0] - 1
+      else:
+         same_score = 0
       rank = data[6]
       status = 'success' if rank <= 564 else 'warning'
       status_text = 'Trúng tuyển' if rank <= 564 else 'Chờ xử lý'
-      html = f"""
+      html_table = f"""
       <tr>
          <td>{data[0]}</td>
          <td>{data[1]}</td>
@@ -38,7 +43,14 @@ def hello(param):
          <td>{data[6]}</td>
          <td><span class="badge bg-{status}">{status_text}</span></td>
       """
-      return make_response(html, 200)
+      html_info = f"""
+         <div class="bd-callout bd-callout-info">
+             Có <span class="fw-bold">{same_score}</span> thí sinh cùng điểm với bạn!
+         </div>
+      """
+      conn.close()
+      return make_response([html_table, html_info], 200)
    
+   conn.close()
    html = '<p class="text-warning">Không tìm thấy kết quả</p>'
-   return html
+   return [html]
